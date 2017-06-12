cc12a4e69c779432acb12d52079a4a14df47b369
AfikNof <afiknof9@gmail.com>
2017-06-13 01:40:23 +0300

    Added server side

--- src/utils/GoogleMap.api.util.js
+++ src/utils/GoogleMap.api.util.js
@@ -7,6 +7,8 @@ const API_KEY_REVERSEGEO = 'AIzaSyD9F3-xzEKrc10lhn0tOQS3ocmhy8UkfxQ';
 const API_KEY_PLACES = 'AIzaSyDhPgHTHDBL5Y40wyTQEVrVMwKvV37GHB4';
 const ROOT_URL_REVERSEGEO = `https://maps.googleapis.com/maps/api/geocode/json?key=${API_KEY_REVERSEGEO}`;
 const ROOT_URL_PLACES = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=500&key=${API_KEY_PLACES}`;
+const API_REVERSEGEO=`http://localhost:3000/api/getlocation?coords=`;
+const API_PLACES=`http://localhost:3000/api/getnearbyplaces?coords=`;
 import axios from 'axios';
 
 const GoogleMapApiUtil = {
@@ -15,12 +17,13 @@ const GoogleMapApiUtil = {
     },
     getAddress: function (coords) {
         const latlng = `${coords.latitude},${coords.longitude}`;
-        return axios.get(`${ROOT_URL_REVERSEGEO}&latlng=${latlng}`);
+        return axios.get(`${API_REVERSEGEO}${latlng}`);
+        // return axios.get(`${ROOT_URL_REVERSEGEO}&latlng=${latlng}`);
     },
     getNearbyPlaces: function (coords) {
         console.log('nearby',coords);
         const latlng = `${coords.latitude},${coords.longitude}`;
-        return axios.get(`${ROOT_URL_PLACES}&latlng=${latlng}`);
+        return axios.get(`${API_PLACES}${latlng}`);
     }
 };
 module.exports = GoogleMapApiUtil;
